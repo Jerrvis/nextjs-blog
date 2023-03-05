22f709aef6963b2e2dd475adf30662b74830b423
Jerrvis <Jerrvis@outlook.com>
2023-03-05 15:12:52 +0800

    debug_day3

--- pages/tech/[slug]/index.tsx
+++ pages/tech/[slug]/index.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: Jerrvis Jerrvis@outlook.com
+ * @Date: 2023-03-05 15:01:54
+ * @LastEditors: Jerrvis Jerrvis@outlook.com
+ * @LastEditTime: 2023-03-05 15:10:44
+ * @FilePath: \nextjs-blog\pages\tech\[slug]\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { getContentBySlug, getTechSlugList } from "@/api";
 import { MDXRemote } from "next-mdx-remote";
 import Head from "next/head";
@@ -26,7 +34,6 @@ export default ({ article }: Props) => {
     });
     lazyImages.forEach((img) => observer.observe(img));
   }, []);
-  console.log(article);
   return (
     <>
       <Head>
